d58b5dba9a1cc8dd718fb4be44dd14877849aa61
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 12:34:37 +0400

    Added Attributes to RegisterAttendeeDto

--- DTOs/RegisterAttendeeDto.cs
+++ DTOs/RegisterAttendeeDto.cs
@@ -2,5 +2,9 @@
 {
     public class RegisterAttendeeDto
     {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+        public int EventId { get; set; }
     }
 }
