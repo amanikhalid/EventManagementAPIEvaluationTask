d332143bceb6805f150a5cd5894d4627aa50d1b0
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 11:39:03 +0400

    Implemented IAttendeeRepository Interface

--- Interfaces/IAttendeeRepository.cs
+++ Interfaces/IAttendeeRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace EventManagementAPIEvaluationTask.Interfaces
+﻿using EventManagementAPIEvaluationTask.Models;
+
+namespace EventManagementAPIEvaluationTask.Interfaces
 {
-    public interface IAttendeeRepository
+    public interface IAttendeeRepository : IGenericRepository<Attendee>
     {
+        Task<IEnumerable<Attendee>> GetAttendeesByEventIdAsync(int eventId);
+        Task<bool> IsEmailRegisteredAsync(string email, int eventId);
+        Task<int> GetAttendeeCountAsync(int eventId);
     }
 }
