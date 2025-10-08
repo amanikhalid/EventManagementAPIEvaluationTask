7bed517f9f5b3aca40f35b33bdfbb80529b63779
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 11:51:11 +0400

    Implemented GenericRepository

--- Repositories/GenericRepository.cs
+++ Repositories/GenericRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace EventManagementAPIEvaluationTask.Repositories
+﻿using EventManagementAPIEvaluationTask.Data;
+using EventManagementAPIEvaluationTask.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EventManagementAPIEvaluationTask.Repositories
 {
-    public class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
     }
 }
