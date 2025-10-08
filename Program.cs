e58c0513f897f4fda8dcc04580a7c6d218c9e0d6
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 11:22:03 +0400

    Modified Program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using EventManagementAPIEvaluationTask.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace EventManagementAPIEvaluationTask
 {
     public class Program
@@ -13,6 +16,9 @@ namespace EventManagementAPIEvaluationTask
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
