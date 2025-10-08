18f270de9580974fea5730c1558c1382b66cec83
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 12:30:42 +0400

    Implemented IAttendeeService Interface

--- Services/IAttendeeService.cs
+++ Services/IAttendeeService.cs
@@ -1,6 +1,10 @@
-﻿namespace EventManagementAPIEvaluationTask.Services
+﻿using EventManagementAPIEvaluationTask.DTOs;
+
+namespace EventManagementAPIEvaluationTask.Services
 {
     public interface IAttendeeService
     {
+        Task<IEnumerable<AttendeeDto>> GetAttendeesByEventIdAsync(int eventId);
+        Task<AttendeeDto> RegisterAttendeeAsync(RegisterAttendeeDto dto);
     }
 }
