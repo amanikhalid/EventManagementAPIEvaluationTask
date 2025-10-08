050d76baba8f458e492bf0994662cda6a094aac8
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 10:52:12 +0400

    Implemented Attendees Attributes

--- Models/Attendee.cs
+++ Models/Attendee.cs
@@ -1,6 +1,25 @@
-﻿namespace EventManagementAPIEvaluationTask.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManagementAPI.Models
 {
     public class Attendee
     {
+        public int AttendeeId { get; set; }
+
+        [Required, MaxLength(80)]
+        public string FullName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+
+        // Foreign Key
+        public int EventId { get; set; }
+
+        // Navigation
+        public Event Event { get; set; }
     }
 }
