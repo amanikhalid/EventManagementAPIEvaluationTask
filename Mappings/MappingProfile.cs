5372c5e2256a8f14df94972bf929ef15a3d93288
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 12:15:01 +0400

    Implemented MappingProfile

--- Mappings/MappingProfile.cs
+++ Mappings/MappingProfile.cs
@@ -1,6 +1,17 @@
-﻿namespace EventManagementAPIEvaluationTask.Mappings
+﻿using AutoMapper;
+using EventManagementAPIEvaluationTask.DTOs;
+using EventManagementAPIEvaluationTask.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EventManagementAPIEvaluationTask.Mappings
 {
-    public class MappingProfile
+    public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventDto>();
+            CreateMap<CreateEventDto, Event>();
+            CreateMap<Attendee, AttendeeDto>();
+        }
     }
 }
