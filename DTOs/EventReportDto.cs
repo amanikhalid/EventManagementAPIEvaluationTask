674f9810e0a3b1f9700faddcdb3574afe00abcd6
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 12:41:27 +0400

    Added Attributes to EventReportDto.cs

--- DTOs/EventReportDto.cs
+++ DTOs/EventReportDto.cs
@@ -2,5 +2,10 @@
 {
     public class EventReportDto
     {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public int AttendeeCount { get; set; }
+        public string? WeatherForecast { get; set; }
     }
 }
