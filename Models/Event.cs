186fafabc693cfc4fa58122d47deddb919745691
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 10:50:56 +0400

    Implemented Events Attributes

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,6 +1,28 @@
-﻿namespace EventManagementAPIEvaluationTask.Models
+﻿using EventManagementAPIEvaluationTask.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventManagementAPI.Models
 {
     public class Event
     {
+        public int EventId { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(300)]
+        public string? Description { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Location { get; set; }
+
+        [Range(10, 500)]
+        public int MaxAttendees { get; set; }
+
+        // Navigation
+        public ICollection<Attendee> Attendees { get; set; }
     }
 }
