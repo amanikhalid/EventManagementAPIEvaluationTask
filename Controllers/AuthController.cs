fb748cefc7d50c445218f1d841c2a5470a0c6848
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 12:56:11 +0400

    Implemented AuthController.cs

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,28 @@
-﻿namespace EventManagementAPIEvaluationTask.Controllers
+﻿using EventManagementAPIEvaluationTask.DTOs;
+using EventManagementAPIEvaluationTask.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagementAPIEvaluationTask.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
     {
+        private readonly IAuthService _auth;
+
+        public AuthController(IAuthService auth)
+        {
+            _auth = auth;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginDto dto)
+        {
+            if (!_auth.ValidateUser(dto))
+                return Unauthorized(new { message = "Invalid credentials" });
+
+            var token = _auth.GenerateToken(dto);
+            return Ok(new { token });
+        }
     }
 }
