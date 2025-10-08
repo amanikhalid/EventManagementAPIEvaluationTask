cd8fa7a840f6bf72c1682b75bf322c6ee064ddb9
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 11:30:21 +0400

    Modified Database

---
+++ Migrations/20251008072905_SeedWithStaticDate.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EventManagementAPIEvaluationTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedWithStaticDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MaxAttendees = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attendees",
+                columns: table => new
+                {
+                    AttendeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RegisteredAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attendees", x => x.AttendeeId);
+                    table.ForeignKey(
+                        name: "FK_Attendees_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "EventId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "EventId", "Date", "Description", "Location", "MaxAttendees", "Title" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 11, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "AI & Robotics", "Muscat", 200, "Tech Conference" },
+                    { 2, new DateTime(2025, 12, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "New Startups", "Dubai", 100, "Startup Pitch" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendees_EventId",
+                table: "Attendees",
+                column: "EventId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attendees");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
