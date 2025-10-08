82b0288724c91e0205ba30aeb8e8029e99aee04e
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 12:43:58 +0400

    Implemented EventReportService.cs

--- Services/EventReportService.cs
+++ Services/EventReportService.cs
@@ -1,6 +1,53 @@
-﻿namespace EventManagementAPIEvaluationTask.Services
+﻿using EventManagementAPIEvaluationTask.DTOs;
+using EventManagementAPIEvaluationTask.Interfaces;
+
+namespace EventManagementAPIEvaluationTask.Services
 {
-    public class EventReportService
+    public interface IEventReportService
     {
+        Task<IEnumerable<EventReportDto>> GetUpcomingEventsAsync();
+    }
+
+    public class EventReportService : IEventReportService
+    {
+        private readonly IEventRepository _eventRepo;
+        private readonly IAttendeeRepository _attendeeRepo;
+        private readonly IWeatherService _weather;
+
+        public EventReportService(
+            IEventRepository eventRepo,
+            IAttendeeRepository attendeeRepo,
+            IWeatherService weather)
+        {
+            _eventRepo = eventRepo;
+            _attendeeRepo = attendeeRepo;
+            _weather = weather;
+        }
+
+        public async Task<IEnumerable<EventReportDto>> GetUpcomingEventsAsync()
+        {
+            var fromDate = DateTime.UtcNow;
+            var toDate = fromDate.AddDays(30);
+
+            var events = await _eventRepo.FindAsync(e => e.Date >= fromDate && e.Date <= toDate);
+            var reports = new List<EventReportDto>();
+
+            foreach (var ev in events)
+            {
+                var attendeeCount = await _attendeeRepo.GetAttendeeCountAsync(ev.EventId);
+                var weather = await _weather.GetWeatherForecastAsync(ev.Location, ev.Date);
+
+                reports.Add(new EventReportDto
+                {
+                    Title = ev.Title,
+                    Date = ev.Date,
+                    Location = ev.Location,
+                    AttendeeCount = attendeeCount,
+                    WeatherForecast = weather
+                });
+            }
+
+            return reports;
+        }
     }
 }
