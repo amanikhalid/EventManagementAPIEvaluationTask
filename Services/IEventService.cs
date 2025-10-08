714968bcb1b9ce0c22f9e06ccc4a3a52dceeaf38
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 12:02:22 +0400

    Implemented IEventService Interface

--- Services/IEventService.cs
+++ Services/IEventService.cs
@@ -1,6 +1,11 @@
-﻿namespace EventManagementAPIEvaluationTask.Services
+﻿using EventManagementAPIEvaluationTask.DTOs;
+
+namespace EventManagementAPIEvaluationTask.Services
 {
     public interface IEventService
     {
+        Task<IEnumerable<EventDto>> GetAllEventsAsync();
+        Task<EventDto?> GetEventByIdAsync(int id);
+        Task<EventDto> CreateEventAsync(CreateEventDto dto);
     }
 }
