a61bb49ab0325404bd2c9ef42a19c993111a4c4e
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 13:02:58 +0400

    Modified Structure

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -1,6 +1,6 @@
 ﻿using EventManagementAPIEvaluationTask.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EventManagementAPIEvaluationTask.Controllers
 {
