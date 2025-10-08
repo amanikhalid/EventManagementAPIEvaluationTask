a53c78f190e29ac4e83e5fea08dc8795e2064000
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 12:13:42 +0400

    Added Attributes to AttendeeDto

--- DTOs/AttendeeDto.cs
+++ DTOs/AttendeeDto.cs
@@ -2,5 +2,8 @@
 {
     public class AttendeeDto
     {
+        public int AttendeeId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
     }
 }
