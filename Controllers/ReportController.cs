295283cf013957919040bf81a9d1d8b30d53596e
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 12:46:21 +0400

    Implemented ReportController.cs

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -1,6 +1,25 @@
-﻿namespace EventManagementAPIEvaluationTask.Controllers
+﻿using EventManagementAPIEvaluationTask.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagementAPIEvaluationTask.Controllers
 {
-    public class ReportController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReportController : ControllerBase
     {
+        private readonly IEventReportService _reportService;
+
+        public ReportController(IEventReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        // GET: api/report/upcoming
+        [HttpGet("upcoming")]
+        public async Task<IActionResult> GetUpcomingEvents()
+        {
+            var result = await _reportService.GetUpcomingEventsAsync();
+            return Ok(result);
+        }
     }
 }
