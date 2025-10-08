746b3f0ae58489a935ee3d4131001422bc84df26
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 12:12:21 +0400

    Added Attributes to EventDto

--- DTOs/EventDto.cs
+++ DTOs/EventDto.cs
@@ -2,5 +2,13 @@
 {
     public class EventDto
     {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public int MaxAttendees { get; set; }
+
+        public List<AttendeeDto> Attendees { get; set; }
     }
 }
