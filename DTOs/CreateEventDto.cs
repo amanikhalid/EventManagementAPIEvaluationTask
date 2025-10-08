005c81ec1e8e45427901819132186d3251418175
Amani Khalid <amanikhalidd@gmail.com>
2025-10-08 12:09:12 +0400

    Added Attributes to CreateEventDto

--- DTOs/CreateEventDto.cs
+++ DTOs/CreateEventDto.cs
@@ -2,5 +2,10 @@
 {
     public class CreateEventDto
     {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public int MaxAttendees { get; set; }
     }
 }
